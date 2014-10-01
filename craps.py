0223e51a67f364cc5637707bbcfdac2ee88bdb18
jkolesar1 <jk323130@wne.edu>
2014-10-01 16:38:46 -0400

    Commented changes

--- craps.py
+++ craps.py
@@ -1,21 +1,21 @@
 from random import randint
 
 print('come-out')
-r1 = randint(1, 6)
-r2 = randint(1, 6)
-v = r1 + r2
+r1 = randint(1, 6)#rename variable to firstDice
+r2 = randint(1, 6)#rename variable to secondDice
+v = r1 + r2 #rename variable v to comeOutValue
 print(v)
-if v == 7 or v == 11:
+if v == 7 or v == 11: #add constants to replace magic numbers 7 and 11
     print('You win!')
-elif v == 2 or v == 3 or v == 12:
+elif v == 2 or v == 3 or v == 12: #change magic numbers again
     print('Craps, you lose.')
 else:
     print('The point is', v)
     r1 = randint(1, 6)
     r2 = randint(1, 6)
-    w = r1 + r2
+    w = r1 + r2 #change w to current roll
     print(w)
-    while w != v and w != 7:
+    while w != v and w != 7: 
         r1 = randint(1, 6)
         r2 = randint(1, 6)
         w = r1 + r2
