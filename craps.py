5a8e796e563330ea17eca277c24ee540ae86e6c4
jkolesar1 <jk323130@wne.edu>
2014-10-01 16:49:46 -0400

    Second changes

--- craps.py
+++ craps.py
@@ -1,26 +1,31 @@
 from random import randint
+#NOT COMPLETE
+Win_Values =[7,11]
+Lose_Values = [2,3,12]
 
 print('come-out')
-r1 = randint(1, 6)#rename variable to firstDice
-r2 = randint(1, 6)#rename variable to secondDice
-v = r1 + r2 #rename variable v to comeOutValue
-print(v)
-if v == 7 or v == 11: #add constants to replace magic numbers 7 and 11
-    print('You win!')
-elif v == 2 or v == 3 or v == 12: #change magic numbers again
-    print('Craps, you lose.')
-else:
-    print('The point is', v)
-    r1 = randint(1, 6)
-    r2 = randint(1, 6)
-    w = r1 + r2 #change w to current roll
-    print(w)
-    while w != v and w != 7: 
-        r1 = randint(1, 6)
-        r2 = randint(1, 6)
-        w = r1 + r2
-        print(w)
-    if w == 7:
+firstDice = randint(1, 6)#rename variable to firstDice
+secondDice = randint(1, 6)#rename variable to secondDice
+comeOutValue = firstDice + secondDice #rename variable v to comeOutValue
+print(comeOutValue)
+for e in Win_Values:
+    if comeOutValue = e:#add constants to replace magic numbers 7 and 11
+        print('You win!')
+        
+    elif v == 2 or v == 3 or v == 12: #change magic numbers again
+        print('Craps, you lose.')
+    else:
+        print('The point is', comeOutValue)
+        firstDice = randint(1, 6)
+        secondDice = randint(1, 6)
+        currrentRoll = firstDice + secondDice #change w to currentRoll
+        print(currentRoll)
+    while currentRoll != comeOutValue and currentRoll != 7: 
+        firstDice = randint(1, 6)
+        secondDice = randint(1, 6)
+        currrentRoll = firstDice + secondDice
+        print(currentRoll)
+    if currentRoll == 7:
         print('Seven out, you lose.')
     else:
         print('Pass, you win!')
